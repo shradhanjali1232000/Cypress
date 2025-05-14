c3d167b9ad19e0fcba494fd10cf03be04326376e
Shradhanjali Swain <shradhaswain290@gmail.com>
2025-05-14 20:13:27 +0530

    commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -25,3 +25,44 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
 /// <reference types="cypress" />
+
+/*Cypress.Commands.add('login',(Username,Password)=>{
+
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get("input[placeholder='Username']").type(Username)
+    cy.get("input[placeholder='Password']").type(Password)
+    cy.get("button[type='submit']").click()
+
+})*/
+ 
+/*Cypress.Commands.add('getIframeBody', (iframe) => {
+    return cy
+      .get(iframe)
+      .its('0.contentDocument.body')
+      .should('not.be.empty')
+      .then(cy.wrap);
+  });*/
+
+
+  Cypress.Commands.add('fillForm', (data) => {
+    cy.get('#firstName').type(data.firstName);
+    cy.get('#lastName').type(data.lastName);
+    cy.get('#userEmail').type(data.email);
+    cy.contains('.custom-control-label', data.gender).click();
+    cy.get('#userNumber').type(data.mobile);
+  
+    // Subject input
+    cy.get('#subjectsInput').type(`${data.subject}{enter}`);
+  
+    // Hobbies
+    cy.contains('.custom-control-label', data.hobby).click();
+  
+    // Address
+    cy.get('#currentAddress').type(data.address);
+  
+    // State and City
+    cy.get('#state').click().contains('div', data.state).click();
+    cy.get('#city').click().contains('div', data.city).click();
+  });
+  
+  
